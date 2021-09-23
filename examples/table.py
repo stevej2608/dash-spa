5115311abe73f9f5ce897f99783e7101b9763d36
Steve Jones <jonesst2608@gmail.com>
2021-09-23 19:29:29 +0100

    Checkbox table example now working

--- examples/table.py
+++ examples/table.py
@@ -1,19 +1,24 @@
-from utils import logging, log
-from dash import html
-import dash_datatables as ddt
+import json
+from utils import logging
 
+from dash import html
+from dash.dependencies import Input, Output
+
+import dash_datatables as ddt
 from dash_spa import spa, Blueprint
 
 from app import app as dash_app
 from server import serve_app
+import pandas as pd
 
 test = Blueprint('test')
 
-import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 
-column_defs = [{"title": i, "data": i} for i in df.columns]
+df.insert(0, '', '')
+
+columns = [{"title": i, "data": i} for i in df.columns]
 
 @test.route('/page1')
 def route1():
@@ -21,9 +26,21 @@ def route1():
 
     table = html.Div([
         html.H2('US Solar Capacity'),
-        html.Br(),
         ddt.DashDatatables(
-            columns=column_defs,
+            id='solar',
+            columns=columns,
+
+            column_defs = [ {
+                'orderable': False,
+                'className': 'select-checkbox',
+                'targets':   0
+                }],
+
+            select = {
+                'style':    'os',
+                'selector': 'td:first-child'
+                },
+
             data=df.to_dict('records'),
             width="100%",
             order=[2, 'asc'],
@@ -31,8 +48,20 @@ def route1():
     ])
 
     return html.Div([
-        table
+        table,
+        html.Div(id='solar#output')        
     ])
+
+@dash_app.callback(Output('solar#output', 'children'), [Input('solar', 'table_event')])
+def display_output(value):
+    selected = None
+
+    if value and value['action'] == 'selectItems':
+        selected = value['indexes']
+        selected = format(json.dumps(selected))
+
+    print(f'Select rows: {selected}')
+    return f'Select rows: {selected}'
 
 
 def create_app():
