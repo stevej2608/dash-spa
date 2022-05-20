f8d6553a13504301cd86c41ce8f8672f41333f31
Steve Jones <jonesst2608@gmail.com>
2022-05-20 10:23:41 +0100

    Updated setup.py, uploaded package to pypicloud

--- setup.py
+++ setup.py
@@ -1,39 +1,97 @@
-import io
 import os
 import re
 
 from setuptools import find_packages
 from setuptools import setup
 
+HERE = os.path.dirname(os.path.abspath(__file__))
 
-def read(filename):
-    filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u"")
-    with io.open(filename, mode="r", encoding='utf-8') as fd:
-        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+def _get_long_description():
+    with open(os.path.join(HERE, "README.md")) as f:
+        return f.read()
 
+def _get_version():
+    """ Get version by parsing _version programmatically """
+    packages = ["dash_spa"]
+    version_ns = {}
+    with open(
+            os.path.join(HERE, packages[0], "__init__.py")
+    ) as f:
+        exec(f.read(), {}, version_ns)
+    version = version_ns["__version__"]
+    return version
+
+
+def _get_dependencies():
+    """Parse requirements.txt and return a list of dependencies"""
+    with open(os.path.join(HERE, "requirements.txt")) as f:
+        requirements = f.read()
+
+    result = []
+    requirements = requirements.split('\n')
+    for r in requirements:
+        if r == '# setup.py - exclude':
+            break
+        if r == '' or r.startswith('#'):
+            continue
+        result.append(r)
+    return result
+
+def _get_extra_dependencies(extra):
+    """Parse requirements.txt and return a list of the
+    extra dependencies required for 'extra' """
+    with open(os.path.join(HERE, "requirements.txt")) as f:
+        requirements = f.read()
+
+    result = []
+    sections = []
+    requirements = requirements.split('\n')
+    for r in requirements:
+        if r.startswith('# setup.py - extra'):
+            sections = re.split(r'\W+', r)[4:]
+            continue
+        if r == '' or r.startswith('#'):
+            if len(result) > 0:
+                break
+            else:
+                continue
+        if extra in sections:
+            result.append(r)
+    return result
 
 setup(
-    name="dash-pages-spa",
+    name="dash-spa",
     version="0.1.0",
-    url="https://github.com/stevej2608/dash-pages-spa",
+    url="https://github.com/stevej2608/dash-spa",
     license='MIT',
 
     author="Steve Jones",
-    author_email="jonesst2608@gmail.com",
+    author_email="jonesst608@gmail.com",
 
     description="Dash Pages SPA Framework",
-    long_description=read("README.md"),
+    long_description=_get_long_description(),
+    long_description_content_type='text/markdown',
 
-    packages=find_packages(exclude=('tests',)),
-    python_requires='>=3.6'
-    install_requires=[],
+    packages=find_packages(include=[
+        'dash_spa','dash_spa.plugins','dash_spa.utils',
+        'dash_spa_admin', 'dash_spa_admin.views'
+        ]),
+
+    include_package_data=True,
+	python_requires='>=3.6',
+    install_requires=_get_dependencies(),
+    extras_require={
+        "admin": _get_extra_dependencies("dash_spa_admin"),
+    },
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Environment :: Web Environment",
+        "Framework :: Dash",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
