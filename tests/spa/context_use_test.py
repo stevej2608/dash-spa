0ea55d82d0c786ecf6d390607449f08b9db47df0
Steve Jones <jonesst2608@gmail.com>
2022-06-23 11:09:03 +0100

    More context testing

--- tests/spa/context_use_test.py
+++ tests/spa/context_use_test.py
@@ -64,3 +64,23 @@ def test_context_list():
    # Confirm ButtonState dataclass is unchanged
 
     assert SizesState.size == 10
+
+
+def test_context_dict():
+
+    @dataclass
+    class ColourState(ContextState):
+        selected: str = 'red'
+        colours: list = field(default_factory=lambda: ['red', 'green', 'blue'])
+
+    ColourContext = createContext(ColourState);
+
+    @ColourContext.Provider(id='test1')
+    def layout_test1(index):
+        state = ColourContext.getState()
+        assert state.selected == state.colours[index]
+        state.selected = state.colours[index+1]
+        return html.Div()
+
+    for value in [0, 1]:
+        layout_test1(value)
