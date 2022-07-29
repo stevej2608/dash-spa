daf5a3c3879dd4921cbd5ed0fca8cac6eaf4b602
Steve Jones <jonesst2608@gmail.com>
2022-07-29 05:49:55 +0100

    Added buttons_page.py to demo

---
+++ pages/buttons/button_toolbar.py
@@ -0,0 +1,50 @@
+from typing import List, Dict
+from dash import html, ALL
+from dash_spa import prefix
+from dash_spa.logging import log
+from dash_spa.spa_context import  createContext, ContextState, dataclass, EMPTY_LIST
+import dash_spa as spa
+
+
+@dataclass
+class TButton(ContextState):
+    name: str = ''
+    clicks: int = 0
+
+@dataclass
+class TBState(ContextState):
+    title: str = ""
+    buttons: List[TButton] = EMPTY_LIST
+
+    def __post_init__(self):
+        self.buttons = [TButton(name, 0) for name in self.buttons]
+
+ToolbarContext: Dict[str, TBState] = createContext()
+
+def button_toolbar(id, initial_state:TBState):
+    pid = prefix(f'button_toolbar_{id}')
+
+    state, _ = ToolbarContext.useState(id, initial_state=initial_state)
+
+    log.info("button_toolbar state.id=%s (%s)", pid(), state.cid())
+
+    button_match = spa.match({'type': pid('btn'), 'idx': ALL})
+
+    btns = html.Div([
+        html.Button(btn.name, id=button_match.idx(idx), type="button", className='btn btn-secondary me-1')
+        for idx, btn in enumerate(state.buttons)
+        ]
+    )
+
+    msg = [f"{btn.name} pressed {btn.clicks} times" for btn in state.buttons]
+    container = html.H5(", ".join(msg))
+
+    @ToolbarContext.On(button_match.input.n_clicks)
+    def btn_update(clicks):
+        log.info("btn_update state.cid=%s", state.cid())
+        index = spa.trigger_index()
+        state.buttons[index].clicks += 1
+
+    title = html.H4(f"Toolbar {state.title}")
+
+    return html.Div([title, btns, container], style={'background-color': '#e6e6e6'})
---
+++ pages/buttons_page.py
@@ -0,0 +1,72 @@
+from dash import html
+from dash_spa import register_page
+from dash_spa.logging import log
+
+from pages import BUTTONS_PAGE_SLUG
+
+from .buttons.button_toolbar import ToolbarContext, button_toolbar, TBState
+
+page = register_page(__name__, path=BUTTONS_PAGE_SLUG, title="Button Toolbar Page", short_name='Buttons')
+
+# Example of using the Dash/SPA context pattern
+# that allows a components state change to be easily passed
+# between components and trigger UI updates.
+#
+# The example creates two panels, each with two instances of
+# a toolbar component. The buttons in each toolbar update the common
+# ButtonContext for the panel. Because the layout_page() method
+# is decorated with ButtonContext.Provider it  will be
+# called to refresh the UI whenever the context changes.
+
+
+def tb_report(tb: TBState):
+    """reports single toolbar state"""
+
+    msg = [f'{btn.name}={btn.clicks}' for btn in tb.buttons]
+    return html.H4(f'{tb.title}: {", ".join(msg)}')
+
+@ToolbarContext.Provider(id='top', persistent=True)
+def top_panel_layout():
+
+    log.info('top_panel_layout()')
+
+    # Create some toolbars
+
+    main_toolbar = button_toolbar(page.id("top_main"), TBState("main", ['close', "exit", 'refresh']))
+    page_toolbar = button_toolbar(page.id("top_page"), TBState("page", ['next', "prev", 'top', 'bottom']))
+
+    state = ToolbarContext.getState()
+
+    title = html.H3('Toolbar Component Example (with persistent state)')
+
+    report = html.Div([tb_report(tb) for tb in state.items()], style={'background-color': '#e6e6e6'})
+    report.children.insert(0, html.H3('Toolbar Report'))
+
+    return html.Div([title, main_toolbar, page_toolbar, report])
+
+
+@ToolbarContext.Provider('bottom')
+def bottom_panel_layout():
+
+    log.info('bottom_panel_layout()')
+
+    # Create some toolbars
+
+    main_toolbar = button_toolbar(page.id("bottom_main"), TBState("main", ['close', "exit", 'refresh']))
+    page_toolbar = button_toolbar(page.id("bottom_page"), TBState("page", ['next', "prev", 'top', 'bottom']))
+
+    state = ToolbarContext.getState()
+
+    title = html.H3('Toolbar Component Example (without persistent state)')
+
+    report = html.Div([tb_report(tb) for tb in state.items()], style={'background-color': '#e6e6e6'})
+    report.children.insert(0, html.H3('Toolbar Report'))
+
+
+    return html.Div([title, main_toolbar, page_toolbar, report])
+
+def layout():
+    log.info('layout()')
+    top = top_panel_layout()
+    bottom = bottom_panel_layout()
+    return html.Div([top, bottom])
