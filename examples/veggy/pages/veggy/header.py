65d4c15fc1e5606e65c5be5cf86674c21f38c543
Steve Jones <jonesst2608@gmail.com>
2022-08-05 11:43:46 +0100

    splitting veggy UI into components

--- examples/veggy/pages/veggy/header.py
+++ examples/veggy/pages/veggy/header.py
@@ -1,6 +1,7 @@
 from dash import html, dcc
 
-BAG_IMG = 'https://res.cloudinary.com/sivadass/image/upload/v1493548928/icons/bag.png'
+from .cart import cart
+
 VEGGY_IMG = 'https://res.cloudinary.com/sivadass/image/upload/v1493547373/dummy-logo/Veggy.png'
 SEARCH_IMG = 'https://res.cloudinary.com/sivadass/image/upload/v1494756966/icons/search-green.png'
 BACK_ARROW_IMG = 'https://res.cloudinary.com/sivadass/image/upload/v1494756030/icons/back.png'
@@ -24,57 +25,6 @@ def brand():
     return  html.Div([
         html.Img(className='logo', src=VEGGY_IMG, alt='Veggy Brand Logo')
     ], className='brand')
-
-def cart_info(items, total):
-    return  html.Div([
-        html.Table([
-            html.Tbody([
-                html.Tr([
-                    html.Td("No. of items"),
-                    html.Td(":"),
-                    html.Td([
-                        html.Strong(items)
-                    ])
-                ]),
-                html.Tr([
-                    html.Td("Sub Total"),
-                    html.Td(":"),
-                    html.Td([
-                        html.Strong(total)
-                    ])
-                ])
-            ])
-        ])
-    ], className='cart-info')
-
-def cart_preview():
-    return html.Div([
-            html.Div([
-                html.Div([
-                    html.Div([
-                        html.Img(src=BAG_IMG, alt='empty-cart'),
-                        html.H2("You cart is empty!")
-                    ], className='empty-cart')
-                ], style='position: absolute; inset: 0px; overflow: scroll; margin-right: -17px; margin-bottom: -17px;'),
-                html.Div([
-                    html.Div(style='position: relative; display: block; height: 100%; cursor: pointer; border-radius: inherit; background-color: rgba(0, 0, 0, 0.2); width: 0px;')
-                ], style='position: absolute; height: 6px; right: 2px; bottom: 2px; left: 2px; border-radius: 3px;'),
-                html.Div([
-                    html.Div(style='position: relative; display: block; width: 100%; cursor: pointer; border-radius: inherit; background-color: rgba(0, 0, 0, 0.2); height: 0px;')
-                ], style='position: absolute; width: 6px; right: 2px; bottom: 2px; top: 2px; border-radius: 3px;')
-            ], style='position: relative; overflow: hidden; width: 360px; height: 320px;'),
-            html.Div([
-                html.Button("PROCEED TO CHECKOUT", type='button', className='disabled')
-            ], className='action-block')
-        ], className='cart-preview')
-
-def cart():
-    return  html.Div([
-        cart_info(0, 0),
-        html.A(html.Img(className='', src=BAG_IMG, alt='Cart'), className='cart-icon', href='#'),
-        cart_preview()
-    ], className='cart')
-
 
 def header():
     return html.Header([
